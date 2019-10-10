558d426f41726cfe5d3393d6cbb3964ed275b0e3
ezzikone <47362010+ezzikone@users.noreply.github.com>
2019-10-10 13:42:21 +0400

    feat
    success

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor(first = 0, last = 0) {
+        this.first = first;
+        this.last = last;
+    }
 
     setRange(min, max) {
-
+        this.first = min;                               //присваиваем стартовые значения отрезку от min
+        this.last = max;                                //до max
     }
 
     guess() {
-
+        let ans = 0;
+        const i = this.first,
+              j = this.last;
+        ans = Math.ceil((i+j)/2);                    //находим среднее значение (между заданным промежутком) и округляем его в большую сторону
+        return ans;
     }
 
     lower() {
-
+        const i = this.first,
+              j = this.last;
+        this.last = Math.ceil((i+j)/2);             //последняя цифра отрезка становится серединой отрезка, в меньшую сторону
     }
 
     greater() {
-
+        const i = this.first,
+              j = this.last;
+        this.first = Math.ceil((i+j)/2);            //первая цифра отрезка становится серединой отрезка, в меньшую сторону
     }
 }
 
